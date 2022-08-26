fdf0d1c549f48a10177c58a993e18120ddb8ebfa
Andrew Foster <andrewfoster@andrews-mbp-7.unl.edu>
2022-08-26 16:14:01 -0500

    fixed standard library bug.

---
+++ hello.c
@@ -0,0 +1,15 @@
+/**
+ * Basic Hello World Program
+ **/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+ int main (int argc, char **argv) {
+
+    printf("Hello, I'm Andy Foster!\n");
+    printf("My major is computer science!\n");
+
+    return 0;
+ }
+
