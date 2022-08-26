88db3c22b08448c3b2621967192fd955bd1ebe93
Will Schmidt <wschmidt3@huskers.unl.edu>
2022-08-26 21:48:37 +0000

    added Wills info

--- hello.c
+++ hello.c
@@ -10,6 +10,9 @@
     printf("Hello, I'm Andy Foster!\n");
     printf("My major is computer science!\n");
     printf("My minor is Business Administration!\n");
+    printf("Hello I am Will Schmidt\n");
+    printf("I am a computer science major\n");
+
 
     return 0;
  }
